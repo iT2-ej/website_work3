7bfb556e731f8143c34a03c44608ef4eeeb9c0b9
EC2 Default User <ec2-user@ip-172-31-25-67.ap-northeast-1.compute.internal>
2022-10-04 14:11:27 +0000

    9th commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,14 @@
 class BooksController < ApplicationController
   def new
     @book = Book.new
+
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path(@book.id)
+    redirect_to book_path(@book.id),notice: 'You have created book successfully.'
   end
 
   def show
@@ -29,7 +30,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id),notice: 'You have updated book successfully.'
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: '新規登録が完了しました'
+    else
+      render :new
+    end
+  end
+
 
   def index
    @users = User.all
@@ -22,7 +32,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id)
+    redirect_to user_path(@user.id), notice: 'You have updated user successfully.'
 
   end
 
